c589afdb74c17af01b4e598d61645dd96af0d9bc
anoorishad <anoorishad@me.com>
2022-05-26 18:55:26 -0700

    finished sign up functionality

---
+++ public/js/signUp.js
@@ -0,0 +1,26 @@
+const signupFormHandler = async (event) => {
+    event.preventDefault();
+
+    const name = document.querySelector('#signUpName').value.trim();
+    const email = document.querySelector('#signUpEmail').value.trim();
+    const password = document.querySelector('#signUpPassword').value.trim();
+
+    if (name && email && password) {
+        const response = await fetch('/api/users/', {
+            method: 'POST',
+            body: JSON.stringify({ name, email, password }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.ok) {
+            document.location.replace('/profile');
+        } else {
+            alert(response.statusText);
+        }
+    }
+};
+
+
+document
+    .querySelector('.signUpCard')
+    .addEventListener('submit', signupFormHandler);
